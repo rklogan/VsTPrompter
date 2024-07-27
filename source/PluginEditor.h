830ab9a246a7abc3f74cde849b0e2e7a5ecf1eaa
Ryan Logan <ryan.k.logan@gmail.com>
2024-07-27 13:37:00 -0400

    remove template comments from PluginEditor.h

--- source/PluginEditor.h
+++ source/PluginEditor.h
@@ -4,20 +4,16 @@
 #include "BinaryData.h"
 #include "melatonin_inspector/melatonin_inspector.h"
 
-//==============================================================================
 class PluginEditor : public juce::AudioProcessorEditor
 {
 public:
     explicit PluginEditor (PluginProcessor&);
     ~PluginEditor() override;
 
-    //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
     PluginProcessor& processorRef;
     std::unique_ptr<melatonin::Inspector> inspector;
     juce::TextButton inspectButton { "Inspect the UI" };
